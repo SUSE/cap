fbd945199f3585f9caf86ea528ac259ec84e65be
Andreas Kupries <andreas.kupries@suse.com>
2020-04-07 12:50:37 -0700

    [jira#cap-1353] Extended pipeline creator and template with parametrization of brain tests.

--- cap-ci/create_cap-release_pipeline.sh
+++ cap-ci/create_cap-release_pipeline.sh
@@ -11,9 +11,18 @@ export BACKEND="${BACKEND:-backend: [ caasp4, aks, gke, eks ]}"
 export OPTIONS="${OPTIONS:-options: [ sa, ha, all ]}"
 export EIRINI="${EIRINI:-eirini: [ diego, eirini ]}"
 
+export BRAIN_VERBOSE="${BRAIN_VERBOSE:-false}"
+export BRAIN_INORDER="${BRAIN_INORDER:-false}"
+export BRAIN_INCLUDE="${BRAIN_INCLUDE:-}"
+export BRAIN_EXCLUDE="${BRAIN_EXCLUDE:-}"
+
 gomplate -d 'BACKEND=env:///BACKEND?type=application/yaml' \
          -d 'OPTIONS=env:///OPTIONS?type=application/yaml' \
          -d 'EIRINI=env:///EIRINI?type=application/yaml' \
+         -d 'BRAIN_VERBOSE=env:///BRAIN_VERBOSE' \
+         -d 'BRAIN_INORDER=env:///BRAIN_INORDER' \
+         -d 'BRAIN_INCLUDE=env:///BRAIN_INCLUDE' \
+         -d 'BRAIN_EXCLUDE=env:///BRAIN_EXCLUDE' \
          -f pipeline.template > "$PIPELINE".yaml
 
 fly -t concourse.suse.dev sp -c "$PIPELINE".yaml -p "$PIPELINE"
