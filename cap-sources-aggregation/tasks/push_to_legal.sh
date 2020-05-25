03665a5cd19ca6268ce43d737de36b991363397a
Christian Richter <crichter@suse.com>
2020-05-25 09:17:40 +0200

    Add 'legal-auto' group to make legal review work
    Signed-off-by: André Duffeck <aduffeck@suse.com>

--- cap-sources-aggregation/tasks/push_to_legal.sh
+++ cap-sources-aggregation/tasks/push_to_legal.sh
@@ -23,6 +23,7 @@ cat <<HEREDOC | osc meta prj -F - "$TARGET_OBS_PROJECT"
   <title>BOSH release sources for CAP ${CAP_VERSION}</title>
   <description/>
   <person userid="alfred-jenkins" role="maintainer"/>
+  <group groupid="legal-auto" role="reviewer"/>
 </project>
 HEREDOC
 
