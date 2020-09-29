533bf0891f01d783598949d96189df886a14f3b9
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-09-29 17:07:27 -0500

    fix: split long lines
    Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

--- cap-ci/create_pipeline.sh
+++ cap-ci/create_pipeline.sh
@@ -2,9 +2,12 @@
 
 set -o errexit -o nounset -o pipefail
 
-# NOTE for now, add your cluster's kubeconfig in git@github.com:SUSE/cf-ci-pools.git on branch ${BACKEND}-kube-hosts in unclaimed folder
+# NOTE for now, add your cluster's kubeconfig in 
+# git@github.com:SUSE/cf-ci-pools.git in the ${BACKEND}-kube-hosts branch under
+# the unclaimed folder.
 # Usage example: ./create_pipeline.sh <concourse-target> <pipeline-name>>
-# You can configure all the required values in config yamls: cap-pre-release.yaml, cap-release.yaml or your own.
+# You can configure all the required values in config yamls:
+# cap-pre-release.yaml, cap-release.yaml or your own.
 
 if ! hash gomplate 2>/dev/null; then
     >&2 echo "ERROR: 'gomplate' missing. https://docs.gomplate.ca/installing/."
@@ -54,15 +57,20 @@ fly_args=(
     "--pipeline=${PIPELINE}"
 )
 
-# space-separated paths to template files and directories which contain template files
+# Space-separated paths to template files and directories which contain template
+# files.
 template_paths="scripts jobs common"
 templates=$(find ${template_paths} -type f -exec echo "--template="{} \;)
 pipeline_file=${3:-pipeline.yaml.tmpl}
 
 # Determine if the pipeline being pushed is a new pipeline
 existing_pipeline_job_count=$(
-  fly --target ${target} get-pipeline --pipeline ${PIPELINE} --json | jq '.jobs | length'
-)
+  fly \
+    --target ${target} \
+    get-pipeline \
+    --pipeline ${PIPELINE} \
+    --json \
+    | jq '.jobs | length')
 if [[ ${existing_pipeline_job_count} -gt 0 ]]; then
   pipeline_already_existed=true
 else
@@ -70,14 +78,25 @@ else
 fi
 
 # Push a new pipeline, or update an existing one
-fly "${fly_args[@]}" --config <(gomplate --verbose --datasource config="${pipeline_config}" ${templates} --file "${pipeline_file}")
+fly "${fly_args[@]}" --config <(
+  gomplate \
+    --verbose \
+    --datasource config="${pipeline_config}" \
+    ${templates} \
+    --file "${pipeline_file}")
 
-# If the pipeline being pushed was a *new* pipeline, pause all the 'initial' jobs (jobs without 'passed:' dependencies)
-# Important caveat: If a pipeline is updated to add new targets, the new initial jobs will *not* be paused
+# If the pipeline being pushed was a *new* pipeline, pause all the 'initial'
+# jobs (jobs without 'passed:' dependencies).
+# Important caveat: if a pipeline is updated to add new targets, the new initial
+# jobs will *not* be paused.
 if ! ${pipeline_already_existed}; then
   jobs_without_dependencies_names=$(
-    fly ${target:+"--target=${target}"} get-pipeline --json -p "${PIPELINE}" | jq -r '.jobs - [.jobs[] | select(.plan[] | .passed)] | .[].name'
-  )
+    fly \
+      ${target:+"--target=${target}"} \
+      get-pipeline \
+      --json \
+      --pipeline "${PIPELINE}" \
+      | jq -r '.jobs - [.jobs[] | select(.plan[] | .passed)] | .[].name')
   for job_name in ${jobs_without_dependencies_names}; do
     fly ${target:+"--target=${target}"} pause-job -j "${PIPELINE}/${job_name}"
   done
